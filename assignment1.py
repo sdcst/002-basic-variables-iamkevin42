8f965d70bcf3b4afb4db3f0ad6d6c852861f5453
iamkevin42 <113134296+iamkevin42@users.noreply.github.com>
2024-04-12 13:28:21 -0700

    variables

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,13 @@
 #! python3
 
 # SD Computing Studies Assignment
+"""
+Task 1
+(2 points)
+Open the file assignment1.py
+Create 2 variables named x and y. Assign a value of 10 to x and a value of 2.4 to y
+Output is not necessary. The contents of the variables will be checked from within the program.
+"""
+
+x=10
+y=2.4
---
+++ assignment2.py
@@ -0,0 +1,14 @@
+"""
+Task 2
+(2 points)
+Create a file called assignment2.py
+Create variables fname and lname
+Assign the string "Mr" to fname and the string "Yang" to lname
+Output is not necessary. The contents of the variables will be checked from within the program.
+"""
+
+
+fname = "Mr"
+
+Iname = "Yang"
+
